6989df11a0fda24c638921ab610f79527e50f443
HeBianGu <908293466@qq.com>
2024-05-23 16:13:12 +0800

    +++

---
+++ Source/Themes/HeBianGu.Themes.Default/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using Avalonia.Metadata;
+using HeBianGu.Avalonia.Extensions.ApplicationBase;
+
+[assembly: ApplicationResourceLoader]
+
+[assembly: XmlnsDefinition("https://github.com/HeBianGu", "HeBianGu.Themes.Default")]
+[assembly: XmlnsPrefix("https://github.com/HeBianGu", "h")]
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "HeBianGu.Themes.Default")]
+[assembly: XmlnsPrefix("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "h")]
---
+++ Source/Themes/HeBianGu.Themes.Default/StringResourceKey.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Markup.Xaml;
+using System;
+
+namespace HeBianGu.Themes.Default
+{
+    public class StringResourceKey : MarkupExtension
+    {
+        public StringResourceKey()
+        {
+                
+        }
+        public StringResourceKey(Type type, string resourceId)
+        {
+            this.TypeInTargetAssembly = type;
+            this.ResourceId = resourceId;
+        }
+
+        public string ResourceId { get; set; }
+        public Type TypeInTargetAssembly { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return $"S.{this.TypeInTargetAssembly.Name}.{ResourceId}";
+        }
+    }
+}
