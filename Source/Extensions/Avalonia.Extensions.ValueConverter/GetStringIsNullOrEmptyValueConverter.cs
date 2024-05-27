56b6d6d41efc3283bfec760c03d880cc6802ad51
HeBianGu <908293466@qq.com>
2024-05-27 19:04:32 +0800

    ++

--- Source/Extensions/Avalonia.Extensions.ValueConverter/GetStringIsNullOrEmptyValueConverter.cs
+++ Source/Extensions/Avalonia.Extensions.ValueConverter/GetStringIsNullOrEmptyValueConverter.cs
@@ -1,8 +1,56 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Theme;
+using System;
 using System.Globalization;
 
 namespace Avalonia.Extensions.ValueConverter
 {
+    public class GetLevelBrushValueConverter : MarkupValueConverterBase
+    {
+        public override object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is int v)
+            {
+                {
+                    if (v == 0 && Avalonia.Application.Current.TryFindResource(BrushKeys.Purple, out object result))
+                    {
+                        if (result is Brush brush)
+                            return brush;
+                    }
+                }
+                {
+                    if (v == 1 && Avalonia.Application.Current.TryFindResource(BrushKeys.Red, out object result))
+                    {
+                        if (result is Brush brush)
+                            return brush;
+                    }
+                }
+                {
+                    if (v == 2 && Avalonia.Application.Current.TryFindResource(BrushKeys.Orange, out object result))
+                    {
+                        if (result is Brush brush)
+                            return brush;
+                    }
+                }
+                {
+                    if (v == 3 && Avalonia.Application.Current.TryFindResource(BrushKeys.Blue, out object result))
+                    {
+                        if (result is Brush brush)
+                            return brush;
+                    }
+                }
+                {
+                    if (v == 4 && Avalonia.Application.Current.TryFindResource(BrushKeys.Green.ResourceId, out object result))
+                    {
+                        if (result is Brush brush)
+                            return brush;
+                    }
+                }
+            }
+            return value;
+        }
+    }
     public class GetStringIsNullOrEmptyValueConverter : MarkupValueConverterBase
     {
         public object TrueValue { get; set; }
