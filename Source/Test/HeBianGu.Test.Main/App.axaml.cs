db06f99865564059b09bd23109c763d2adda43f6
HeBianGu <908293466@qq.com>
2024-05-28 15:02:00 +0800

    1.0.0

--- Source/Test/HeBianGu.Test.Main/App.axaml.cs
+++ Source/Test/HeBianGu.Test.Main/App.axaml.cs
@@ -38,13 +38,13 @@ public partial class App : ApplicationBase
     {
         base.ConfigureServices(services);
         services.AddSingleton<IMyIoc, MyIoc>();
-        //services.AddSingleton<ILoginWindow, LoginWindow>();
-        //services.AddLoginViewPresenter(x => x.Product = "登陆页面");
+        services.AddSingleton<ILoginWindow, LoginWindow>();
+        services.AddLoginViewPresenter(x => x.Product = "登陆页面");
         services.AddSingleton<IDialogMessageService, AdornerDialogMessageService>();
         services.AddFormMessageService();
         services.AddNoticeMessage();
         services.AddSnackMessage();
-        //services.AddSplashScreen(x => x.Product = "启动页面");
+        services.AddSplashScreen(x => x.Product = "启动页面");
         services.AddTestLoginService();
 
         //  Do ：操作日志
