03193a3f12a70eff76ed39b8a9f9992dab03c5b1
HeBianGu <908293466@qq.com>
2024-05-29 23:21:57 +0800

    ++

--- Source/Styles/Avalonia.Styles.Extension/Resources/GroupBox.axaml.cs
+++ Source/Styles/Avalonia.Styles.Extension/Resources/GroupBox.axaml.cs
@@ -1,16 +1,10 @@
-﻿using Avalonia.Controls;
-using Avalonia.Extensions.ResourceKey;
-using Avalonia.Media;
-using Avalonia.Theme;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia.Controls.Primitives;
+using System;
 
 namespace Avalonia.Styles.Extension
 {
-    public static class GroupBoxKeys
+    public class GroupBox : HeaderedContentControl
     {
-        public static StringResourceKey Default => new StringResourceKey(typeof(GroupBoxKeys), nameof(Default));
+        protected override Type StyleKeyOverride => typeof(GroupBox);
     }
 }
---
+++ Source/Styles/Avalonia.Styles.Extension/Resources/MultiComboBox.axaml.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia.Controls;
+using Avalonia.Extensions.ResourceKey;
+using Avalonia.Media;
+using Avalonia.Mvvm;
+using Avalonia.Theme;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.Styles.Extension
+{
+    public class MultiComboBox : ComboBox
+    {
+        protected override Type StyleKeyOverride => typeof(MultiComboBox);
+
+    }
+
+    public static class MultiComboBoxCommands
+    {
+        //public static IRelayCommand<object, bool> RemoveCommand => new RelayCommand<object, bool> < object, bool>(x =>
+        //{
+        //    if (x is ListBoxItem listBoxItem && listBoxItem.Parent is ListBox listBox && listBox.TemplatedParent is MultiComboBox multiComboBox)
+        //    {
+        //        if (listBox.ItemsSource is IList list)
+        //        {
+        //            list.Remove(listBoxItem.Content);
+        //        }
+        //    }
+        //    return true;
+        //});
+
+        public static IRelayCommand RemoveCommand => null;
+    }
+}
