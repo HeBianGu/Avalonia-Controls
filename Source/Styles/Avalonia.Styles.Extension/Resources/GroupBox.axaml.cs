ca0b0fb5dc81a37035587e2ad5ae3122144a2bb2
HeBianGu <908293466@qq.com>
2024-05-29 22:40:51 +0800

    +

---
+++ Source/Styles/Avalonia.Styles.Extension/Resources/GroupBox.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls;
+using Avalonia.Extensions.ResourceKey;
+using Avalonia.Media;
+using Avalonia.Theme;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avalonia.Styles.Extension
+{
+
+    public static class GroupBoxKeys
+    {
+        public static StringResourceKey Default => new StringResourceKey(typeof(Button), nameof(Default));
+    }
+}
